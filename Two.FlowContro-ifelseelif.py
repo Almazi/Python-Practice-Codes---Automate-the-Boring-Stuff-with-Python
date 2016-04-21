c00494e8332a78ca308d1e4097a0fb6ea9b69c1e
Almazi <navilalmazi@gmail.com>
2016-04-22 01:52:57 +0600

    Updated the loop codes

--- Two.FlowContro-ifelseelif.py
+++ Two.FlowContro-ifelseelif.py
@@ -8,3 +8,20 @@ print("True or True: ",True or True)
 print("True or False:",True or False)
 print("False or True: ",False or True)
 print("False or False: ",False or False)
+name = input("Whats your name: ")
+if name == "bob":
+    print("hello bob!")
+else:
+    print("You are not bob, you are:",name,"dame you!")
+age = int(input("Whats your age?: "))
+if age <= 10:
+    print("You are a kid",name)
+elif age > 10 and age <20:
+    print("Yo ar a teenager",name)
+elif age >= 20 :
+    print("here comes the pressure of world")
+else:
+    print("You are nothing")
+print("Bool checker")
+print("Blank input is 0 or False in bool: ")
+bool(' ')
---
+++ Two.FlowControl-loops.py
@@ -0,0 +1,22 @@
+print("Loooooop")
+
+inputString = int(input("Give me a number, any number, I will iterate that many times: "))
+while inputString > 0:
+    print("Iterate step: ",inputString)
+    inputString -= 1
+name = input("Please enter your name: ")
+while name == '':
+    name = input("You didnt enter anything as your name, please do it: ")
+else:
+    print("Thank you for entering your name: ",name)
+    option = int(input("If you would like to change the entery please press 1, or just press 0 if you are done"))
+    if option == 1 :
+        name = input("Please enter your name: ")
+        while name == '':
+            name = input("You didnt enter anything as your name, please do it: ")
+        else:
+            print("Thank you for entering your name: ",name)
+            option = int(input("If you would like to change the entery please press 1, or just press 0 if you are done"))
+    elif option == 0:
+        print("Final input of your name: ",name)
+        
