1f6c098182ea7e0863492e9f8d883e3d6396702a
Almazi <navilalmazi@gmail.com>
2016-04-22 07:10:52 +0600

    Finished loop code starting function

--- Two.FlowControl-loops.py
+++ Two.FlowControl-loops.py
@@ -19,4 +19,7 @@ else:
             option = int(input("If you would like to change the entery please press 1, or just press 0 if you are done"))
     elif option == 0:
         print("Final input of your name: ",name)
-        
+
+
+for variable in range(5):
+    print(variable)
