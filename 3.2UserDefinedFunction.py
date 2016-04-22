4db00b56c614d848bce24a0980013d2a5bee9d24
Almazi <navilalmazi@gmail.com>
2016-04-23 00:42:54 +0600

    3.2User defined function code

---
+++ 3.2UserDefinedFunction.py
@@ -0,0 +1,6 @@
+def square(number):
+    return number**2
+
+number = int(input("Give me a number to square: "))
+print("The square of", number, "is", square(number))
+print("Every function has a return type, no matter it is mentioned or not. Like print function's return type is None type which returns nothing")
